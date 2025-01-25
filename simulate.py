97398e7092b04c7ef07b3ae5d3aea1b9966bfa5b
lilamcguirk <lmcguirk@uvm.edu>
2025-01-25 16:18:56 -0500

    Added pybullett data

--- simulate.py
+++ simulate.py
@@ -1,7 +1,9 @@
+import pybullet_data
 import pybullet as p
 import time
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
