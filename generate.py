e3a2a41b5f02eb81362c5ceff9d96c8d552fde28
lilamcguirk <lmcguirk@uvm.edu>
2025-01-25 16:26:06 -0500

    Added size variables

---
+++ generate.py
@@ -0,0 +1,12 @@
+import pyrosim.pyrosim as pyrosim
+
+pyrosim.Start_SDF("box.sdf")
+
+length = 1
+width = 1
+height = 1
+
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length, width, height])
+
+pyrosim.End()
+
