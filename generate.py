7ea4088cfd8106322e348458a82544e070f18c2f
lilamcguirk <lmcguirk@uvm.edu>
2025-01-29 15:40:12 -0500

    Changed it back to a single cube at the origin

--- generate.py
+++ generate.py
@@ -6,17 +6,7 @@ length = 1
 width = 1
 height = 1
 
-for row in range(5):
-	for column in range(5):
-		x = row
-		y = column
-		z = 0.5
-		
-		size = 1	
-		for level in range(5):
-			pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[size, size, size])
-			z += 1
-			size *= 0.9
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length, width, height])
 
 pyrosim.End()
 
