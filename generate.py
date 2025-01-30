494ec866cf3d5feb0210e785167cb0c92a2ab3c6
lilamcguirk <lmcguirk@uvm.edu>
2025-01-30 16:07:42 -0500

    Added a function called Create_World.

--- generate.py
+++ generate.py
@@ -1,12 +1,13 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("world.sdf")
+def Create_World():
+	pyrosim.Start_SDF("world.sdf")
 
-length = 1
-width = 1
-height = 1
+	length = 1
+	width = 1
+	height = 1
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length, width, height])
+	pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length, width, height])
 
-pyrosim.End()
+	pyrosim.End()
 
