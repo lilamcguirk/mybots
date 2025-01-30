9cc8ce79f883ffdf9187f76889c29f6dd812220f
lilamcguirk <lmcguirk@uvm.edu>
2025-01-30 16:24:43 -0500

    Added robotID

--- simulate.py
+++ simulate.py
@@ -6,6 +6,7 @@ physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-9.8)
+robotId = p.loadURDF("body.urdf")
 planeId = p.loadURDF("plane.urdf")
 p.loadSDF("world.sdf")
 for i in range(1000):
