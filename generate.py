7d61a0b45b7e8480287f3102e0bc6e3c713cc3ed
lilamcguirk <lmcguirk@uvm.edu>
2025-01-31 13:37:44 -0500

    Added links 3_4 and 4

--- generate.py
+++ generate.py
@@ -30,6 +30,10 @@ def Create_Robot():
 
 	pyrosim.Send_Cube(name="Link3", pos=[0, 0.5, 0], size=[1, 1, 1])
 
+	pyrosim.Send_Joint( name = "Link3_Link4" , parent= "Link3" , child = "Link4" , type = "revolute", position = [0, 1, 0])
+	
+	pyrosim.Send_Cube(name="Link4", pos=[0, 0.5, 0], size=[1, 1, 1])
+
 	pyrosim.End()
 
 Create_World()
