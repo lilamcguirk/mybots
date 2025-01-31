cbec25f6952d9ca53236a8854614273e13a73518
lilamcguirk <lmcguirk@uvm.edu>
2025-01-31 14:39:13 -0500

    Made bot with torso and front and back leg

--- generate.py
+++ generate.py
@@ -12,35 +12,23 @@ def Create_World():
 	pyrosim.End()
 
 def Create_Robot():
+
 	pyrosim.Start_URDF("body.urdf")
 	
-	# torso
-	pyrosim.Send_Cube(name="Link0", pos=[0, 0, 0.5] , size=[1, 1, 1])
+	# torso - root link
+	pyrosim.Send_Cube(name="Link0", pos=[1.5, 0, 1.5] , size=[1, 1, 1])
 
-	pyrosim.Send_Joint( name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0, 0, 1])
-	
-	# leg
-	pyrosim.Send_Cube(name="Link1", pos=[0, 0, 0.5], size=[1, 1, 1])
+	 # torso_backleg - joint
+	pyrosim.Send_Joint(name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [1, 0, 1])
 
-	pyrosim.Send_Joint( name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0, 0, 1])
+	 # back leg - link
+	pyrosim.Send_Cube(name="Link1", pos=[-0.5, 0, -0.5] , size=[1, 1, 1])
 
-	pyrosim.Send_Cube(name="Link2", pos=[0, 0, 0.5], size=[1, 1, 1])
+	# torso_frontleg - joint
+	pyrosim.Send_Joint( name = "Link0_Link2" , parent= "Link0" , child = "Link2" , type = "revolute", position = [2, 0, 1])
 
-	pyrosim.Send_Joint( name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [0, 0.5, 0.5])
-
-	pyrosim.Send_Cube(name="Link3", pos=[0, 0.5, 0], size=[1, 1, 1])
-
-	pyrosim.Send_Joint( name = "Link3_Link4" , parent= "Link3" , child = "Link4" , type = "revolute", position = [0, 1, 0])
-	
-	pyrosim.Send_Cube(name="Link4", pos=[0, 0.5, 0], size=[1, 1, 1])
-
-	pyrosim.Send_Joint( name = "Link4_Link5" , parent= "Link4" , child = "Link5" , type = "revolute", position = [0, 0.5, -0.5])
-
-	pyrosim.Send_Cube(name="Link5", pos=[0, 0, -0.5], size=[1, 1, 1])
-
-	pyrosim.Send_Joint( name = "Link5_Link6" , parent= "Link5" , child = "Link6" , type = "revolute", position = [0, 0, -1])
-
-	pyrosim.Send_Cube(name="Link6", pos=[0, 0, -0.5], size=[1, 1, 1])
+	# front leg - link
+	pyrosim.Send_Cube(name="Link2", pos=[0.5, 0, -0.5] , size=[1, 1, 1])
 	pyrosim.End()
 
 Create_World()
