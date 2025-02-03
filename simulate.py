2014f2312b73226869baf631e465b89255d8e3a1
lilamcguirk <lmcguirk@uvm.edu>
2025-02-03 15:50:52 -0500

    added numpy vector, printed values, and exit()

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet_data
 import pybullet as p
 import time
 import pyrosim.pyrosim as pyrosim
+import numpy
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -11,6 +12,9 @@ robotId = p.loadURDF("body.urdf")
 planeId = p.loadURDF("plane.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = numpy.zeros(10000)
+print(backLegSensorValues)
+exit()
 for i in range(1000):
 	p.stepSimulation()
 	backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("Link1")
