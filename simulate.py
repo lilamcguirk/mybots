08310cd9738cc0bd28af716c057d6213e8d4bd9c
lilamcguirk <lmcguirk@uvm.edu>
2025-02-03 15:08:04 -0500

    Added a sensor to the back leg

--- simulate.py
+++ simulate.py
@@ -11,6 +11,7 @@ planeId = p.loadURDF("plane.urdf")
 p.loadSDF("world.sdf")
 for i in range(1000):
 	p.stepSimulation()
+	backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
 	time.sleep(1/60)
 	print(i)
 
