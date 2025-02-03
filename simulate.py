37d786d331f49177bb91ee18eb0fd959364d9b8c
lilamcguirk <lmcguirk@uvm.edu>
2025-02-03 15:39:45 -0500

    Added print fnction for back leg sensor

--- simulate.py
+++ simulate.py
@@ -10,10 +10,11 @@ p.setGravity(0,0,-9.8)
 robotId = p.loadURDF("body.urdf")
 planeId = p.loadURDF("plane.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
 for i in range(1000):
 	p.stepSimulation()
-	backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+	backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("Link1")
+	print(backLegTouch)
 	time.sleep(1/60)
-	print(i)
 
 p.disconnect()
