583977beecf70fed14bc67c840208ba1059d8602
lilamcguirk <lmcguirk@uvm.edu>
2025-02-05 14:58:22 -0500

    Added plot for front leg sensor

--- analyze.py
+++ analyze.py
@@ -4,5 +4,9 @@ import numpy as np
 backLegSensorValues = np.load('data/back_leg_sensor_values.npy')
 print(backLegSensorValues)
 
+frontLegSensorValues = np.load('data/front_leg_sensor_values.npy')
+print(frontLegSensorValues)
+
 matplotlib.pyplot.plot(backLegSensorValues)
+matplotlib.pyplot.plot(frontLegSensorValues)
 matplotlib.pyplot.show()
