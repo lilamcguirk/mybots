0adf96a634c909a074f7e6f1e6757998fa87776b
lilamcguirk <lmcguirk@uvm.edu>
2025-02-12 17:44:59 -0500

    Created sin wave

--- analyze.py
+++ analyze.py
@@ -1,12 +1,17 @@
 import matplotlib.pyplot
 import numpy as np
 
-backLegSensorValues = np.load('data/back_leg_sensor_values.npy')
-print(backLegSensorValues)
+# backLegSensorValues = np.load('data/back_leg_sensor_values.npy')
+# print(backLegSensorValues)
 
-frontLegSensorValues = np.load('data/front_leg_sensor_values.npy')
-print(frontLegSensorValues)
+# frontLegSensorValues = np.load('data/front_leg_sensor_values.npy')
+# print(frontLegSensorValues)
 
-matplotlib.pyplot.plot(backLegSensorValues)
-matplotlib.pyplot.plot(frontLegSensorValues)
+# matplotlib.pyplot.plot(backLegSensorValues, label="Back Leg Sensor Values", linewidth=4)
+# matplotlib.pyplot.plot(frontLegSensorValues, label="Front Leg Sensor Values")
+# matplotlib.pyplot.legend()
+# matplotlib.pyplot.show()
+
+targetAngles = np.load('data/target_angles.npy')
+matplotlib.pyplot.plot(targetAngles)
 matplotlib.pyplot.show()
