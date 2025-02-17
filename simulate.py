80edabaa7491775f529808ff86a3a5a7f28eee93
lilamcguirk <lmcguirk@uvm.edu>
2025-02-17 12:15:46 -0500

    Motors assignment complete

--- simulate.py
+++ simulate.py
@@ -14,11 +14,28 @@ planeId = p.loadURDF("plane.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
+amplitude_backLeg = np.pi
+frequency_backLeg = 30
+phaseOffset_backLeg = 0
+
+amplitude_frontLeg = np.pi
+frequency_frontLeg = 15
+phaseOffset_frontLeg = np.pi/2
+
 num_iterations = 1000
 
-targetAngles = (np.pi / 4) * np.sin(np.linspace(0, 2 * np.pi, num_iterations))
+x = np.linspace(0, 2*np.pi, 1000)
 
-# np.save('data/target_angles.npy', targetAngles)
+targetAngles_backLeg = amplitude_backLeg * np.sin(frequency_backLeg * x + phaseOffset_backLeg)
+
+
+targetAngles_frontLeg = amplitude_frontLeg * np.sin(frequency_frontLeg * x + phaseOffset_frontLeg)
+
+
+# np.save('data/target_angles_back_leg.npy', targetAngles_backLeg)
+# np.save('data/target_angles_front_leg.npy', targetAngles_frontLeg)
+
+# exit()
 
 backLegSensorValues = np.zeros(num_iterations)
 frontLegSensorValues = np.zeros(num_iterations)
@@ -31,15 +48,15 @@ for i in range(num_iterations):
 	bodyIndex = robotId,
 	jointName = b'Link0_Link1',
 	controlMode = p.POSITION_CONTROL,
-	targetPosition=targetAngles[i],
-	maxForce = 140)
+	targetPosition=targetAngles_backLeg[i],
+	maxForce = 25)
 
 	pyrosim.Set_Motor_For_Joint(
         bodyIndex = robotId,
         jointName = b'Link0_Link2',
         controlMode = p.POSITION_CONTROL,
-        targetPosition=targetAngles[i],
-        maxForce = 140)
+        targetPosition=targetAngles_frontLeg[i],
+        maxForce = 25)
 
 	time.sleep(1/240)
 
