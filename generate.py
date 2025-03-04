b52d80ff381f4156cdde99a0c4f22bda1f00556a
lilamcguirk <lmcguirk@uvm.edu>
2025-03-03 19:52:08 -0500

    Commited generate.py

--- generate.py
+++ generate.py
@@ -11,25 +11,34 @@ def Create_World():
 
 	pyrosim.End()
 
-def Create_Robot():
+def Generate_Body():
 
-	pyrosim.Start_URDF("body.urdf")
-	
-	# torso - root link
-	pyrosim.Send_Cube(name="Link0", pos=[1.5, 0, 1.5] , size=[1, 1, 1])
+    pyrosim.Start_URDF("body.urdf")
 
-	 # torso_backleg - joint
-	pyrosim.Send_Joint(name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [1, 0, 1])
+    pyrosim.Send_Cube(name="Torso", pos=[1, 0, 1.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0.5,0,1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [1.5,0,1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[0.5, 0, -0.5], size=[1, 1, 1])
 
-	 # back leg - link
-	pyrosim.Send_Cube(name="Link1", pos=[-0.5, 0, -0.5] , size=[1, 1, 1])
+    pyrosim.End()
 
-	# torso_frontleg - joint
-	pyrosim.Send_Joint( name = "Link0_Link2" , parent= "Link0" , child = "Link2" , type = "revolute", position = [2, 0, 1])
+def Generate_Brain():
+    pyrosim.Start_NeuralNetwork("brain.nndf")
+    pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+    pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
+    pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
+    pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
+    pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
 
-	# front leg - link
-	pyrosim.Send_Cube(name="Link2", pos=[0.5, 0, -0.5] , size=[1, 1, 1])
-	pyrosim.End()
+    #    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight= 1.5)
+    # pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight = 1.5)
+
+    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight= 0.5)
+    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight = 1.5)
+
+    pyrosim.End()
 
 Create_World()
-Create_Robot()
+Generate_Body()
+Generate_Brain()
