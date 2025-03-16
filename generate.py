1994e9f226b99c27c3a72f3741cb39b1a2c82aba
lilamcguirk <lmcguirk@uvm.edu>
2025-03-16 12:39:52 -0400

    Changed generate.py

--- generate.py
+++ generate.py
@@ -25,18 +25,23 @@ def Generate_Body():
 
 def Generate_Brain():
     pyrosim.Start_NeuralNetwork("brain.nndf")
-    pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
-    pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
-    pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
-    pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
-    pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
-
-    #    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight= 1.5)
-    # pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight = 1.5)
-
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight= 0.5)
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight = 1.5)
-
+    
+    sensor_neurons = {0: "Torso", 1: "BackLeg", 2: "FrontLeg"}
+    motor_neurons = {3: "Torso_BackLeg", 4: "Torso_FrontLeg"}
+    
+    # Create sensor neurons
+    for sensor_id, link_name in sensor_neurons.items():
+        pyrosim.Send_Sensor_Neuron(name=sensor_id, linkName=link_name)
+    
+    # Create motor neurons
+    for motor_id, joint_name in motor_neurons.items():
+        pyrosim.Send_Motor_Neuron(name=motor_id, jointName=joint_name)
+    
+    # Create synapses with weight 1
+    for sensor_id in sensor_neurons.keys():
+        for motor_id in motor_neurons.keys():
+            pyrosim.Send_Synapse(sourceNeuronName=sensor_id, targetNeuronName=motor_id, weight=1)
+    
     pyrosim.End()
 
 Create_World()
